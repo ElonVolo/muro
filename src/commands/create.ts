f8a3157b34c65be625cf960c91be651c07cb8a17
Elon Volow <elonvolo@gmail.com>
2022-04-28 05:00:29 -0400

    Adding git repository actions

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -3,14 +3,35 @@ import { GluegunCommand } from 'gluegun';
 const command: GluegunCommand = {
     name: 'create',
     run: async toolbox => {
-        const { print, git, filesystem } = toolbox;
+        const { print, git, filesystem, parameters, system } = toolbox;
+        
+        if (parameters.array.length === 0) {
+            print.error('\nERROR a config.json file must be specified to create a repository cluster\n');
+            process.exit(1);
+        }
+
         print.info('Generating new git repository cluster...');
 
-        let configPath = filesystem.path(__dirname, '..', '..', 'config.json');
+        // let configPath = filesystem.path(__dirname, '..', '..', 'config.json');
+        let cwdDir = filesystem.cwd();
+        let configPath = filesystem.path(cwdDir, 'config.json');
         let configJson = filesystem.read(configPath, 'json');
 
         for (let repoInfo of configJson['repositories']) {
             git.clone(repoInfo.repo);
+            if (repoInfo?.cloneActions) {
+                filesystem.cwd(repoInfo.rootdirname)
+                for (let action of repoInfo["cloneActions"]) {
+                    if (action['type'] === 'command') {
+                        let cmdArgs = action['args'].join(' ');
+                        let command = `${action['command']} ${cmdArgs}`;
+                        print.info(`Running command ${command}`);
+                        system.run(command);
+                    }
+                }
+                
+                filesystem.cwd('..');
+            }
         }
     },
 }
--- src/commands/update.ts
+++ src/commands/update.ts
@@ -3,20 +3,38 @@ import { GluegunCommand } from 'gluegun';
 const command: GluegunCommand = {
     name: 'update',
     run: async toolbox => {
-        const { print, git, filesystem } = toolbox;
+        const { print, git, filesystem, system } = toolbox;
         print.info('Updating git repository cluster...');
 
-        let configPath = filesystem.path(__dirname, '..', '..', 'config.json');
+        // let configPath = filesystem.path(__dirname, '..', '..', 'config.json');
+        // let configJson = filesystem.read(configPath, 'json');
+
+        let cwdDir = filesystem.cwd();
+        let configPath = filesystem.path(cwdDir, 'config.json');
         let configJson = filesystem.read(configPath, 'json');
+
 
         for (let repoInfo of configJson['repositories']) {
             let statusResult = git.checkStatus(repoInfo);
             if (!statusResult) {
                 print.error(`${repoInfo['name']} has changes that need to be committed`);
             } else {
-                git.update(repoInfo.repo);
+                git.update(repoInfo.repo); 
+                if (repoInfo?.updateActions) {
+                    filesystem.cwd(repoInfo.rootdirname)
+                    for (let action of repoInfo["updateActions"]) {
+                        if (action['type'] === 'command') {
+                            let cmdArgs = action['args'].join(' ');
+                            let command = `${action['command']} ${cmdArgs}`;
+                            print.info(`Running command ${command}`);
+                            system.run(command);
+                        }
+                    }
+                }
             }         
         }
+
+        filesystem.cwd('..');
     },
 }
 
